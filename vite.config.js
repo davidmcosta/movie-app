4efe91165bef9090836703c4d8b442b39e5fecf8
davidmcosta <davidcosta_gtr@hotmail.com>
2025-02-11 01:44:26 +0000

    Final

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://davidmcosta.github.io/movie-app/",
   plugins: [react(), tailwindcss()],
 });
